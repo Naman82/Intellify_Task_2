8831daa4768579ed0f220a00cd07fcb8dfbd35a0
Naman82 <b520031@iiit-bh.ac.in>
2022-08-01 15:08:05 +0530

    Landing page ready

--- src/app/urls.py
+++ src/app/urls.py
@@ -1,3 +1,6 @@
 from django.urls import path
+from app import views
 
-urlpatterns=[]
+urlpatterns=[
+    path('',views.index, name='index'),
+]
--- src/src/settings.py
+++ src/src/settings.py
@@ -58,7 +58,7 @@ ROOT_URLCONF = 'src.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR,'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -120,8 +120,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILESDIRS = [os.path.join(BASE_DIR,"static"),]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'app.User'
