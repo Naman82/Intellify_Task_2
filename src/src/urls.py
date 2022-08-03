1d185f8a1da2ea7ea06f61940c1b510220e42eaa
Naman82 <b520031@iiit-bh.ac.in>
2022-08-03 11:40:53 +0530

    changes media

--- src/src/urls.py
+++ src/src/urls.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import include, path,re_path
+from django.views.static import serve
 from django.conf import settings
 from django.conf.urls.static import static
 
@@ -8,4 +9,5 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('',include('app.urls')),
+    re_path(r'^media/(?P<path>.*)$',serve,{'document_root':settings.MEDIA_ROOT}),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
